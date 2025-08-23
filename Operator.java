a052755a05b45c6235706ffe340b13d89a6899a4
avk0004 <vric.vignesh04@gmail.com>
2025-08-23 21:31:13 +0530

    operatorss

--- Operator.java
+++ Operator.java
@@ -2,14 +2,14 @@ public class Operator {
     public static void main(String[] args){
         int a =10, b =20;
         System.err.println(a+ " "+b);
-        int[] c = {1,2,3,5};
+        int[] c = {1,2,3,4,5};
         System.out.println(c.length);
         System.out.println(c.getClass().getName());
         for (int i =0;i<c.length;i++){
             for (int j =0; j<c.length;j++){
-                if(c[i]+c[j] == 5){
-                    System.out.println(c[i]+"\t");
-                    break;
+                if(c[i]+c[j] == 7){
+                    System.out.println(i+""+c[i]+"\t"+c[j]);
+                    // break;
                 }
                 // System.out.print("\n"+c[j]+"\t"+j);
             }
