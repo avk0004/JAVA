27ec0b142a80482d5bd49182aac87fd806be7b73
avk0004 <vric.vignesh04@gmail.com>
2025-08-24 12:24:19 +0530

    vars file

---
+++ vars.java
@@ -0,0 +1,16 @@
+public class vars{
+    public static void main(String[] args){
+        int a =1000;
+        System.out.println("INT"+a);
+        byte b = 10;
+        System.out.println("byte"+b);
+        double c =10.852514851058620d;
+        System.err.println(c);
+        short e = 5040;
+        System.out.println(e);
+        for(int i =1; i <125;i++){
+            char d = (char) i;
+            System.out.println(d+" "+i);
+        }
+    }
+}
