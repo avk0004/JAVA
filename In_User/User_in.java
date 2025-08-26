ca6da784528868a39518c3c0c3a26a864172b073
avk0004 <vric.vignesh04@gmail.com>
2025-08-26 21:26:38 +0530

    Add new folder and its contents

---
+++ In_User/User_in.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class User_in{
+    public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+        int a = inputs.nextInt();
+        inputs.nextLine();
+        String b = inputs.nextLine();
+        // inputs.nextLine();
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}
