11e905d8dc163d146d5db5c2b80c6eb5dfd73825
avk0004 <vric.vignesh04@gmail.com>
2025-09-15 21:20:13 +0530

    If else one input calculator

---
+++ Self/Day 1/Cal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Cal {
+    public static void main (String[] args){
+        int a = 20;
+        int b = 30;
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.nextLine().charAt(0);
+        if (ch =='+') 
+            System.out.println("add"+(a+b));
+        else if (ch =='-') 
+            System.out.println("Sub"+(a-b));
+        else if (ch =='*') 
+            System.out.println("mul"+(a*b));
+        else if(ch == '/')
+            System.out.println("divi"+(a/b));
+        else if (ch == '%')
+            System.out.println("mod"+(a%b));
+        else
+            System.out.println("Not Vaild");
+
+
+    }
+    
+}
