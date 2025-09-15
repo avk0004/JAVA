08717fe5a02e774f18be45069977a9b175fa6100
avk0004 <vric.vignesh04@gmail.com>
2025-09-15 18:13:53 +0530

    Added folder_name and its contents

--- vars.java
+++ vars.java
@@ -1,16 +1,14 @@
 public class vars{
     public static void main(String[] args){
-        int a =1000;
-        System.out.println("INT"+a);
-        byte b = 10;
-        System.out.println("byte"+b);
-        double c =10.852514851058620d;
-        System.err.println(c);
-        short e = 5040;
-        System.out.println(e);
-        for(int i =1; i <125;i++){
-            char d = (char) i;
-            System.out.println(d+" "+i);
+      for( int i =0; i<=8;i++){
+        if(i == 5){
+            System.out.println(i);
+            break;
         }
+        if(i == 8){
+            System.out.println(i);
+        }
+
+      }
     }
 }
