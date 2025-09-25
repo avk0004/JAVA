fc79e2a43b6abd9ccaa900ea1294ddcfdcc43f73
avk0004 <vric.vignesh04@gmail.com>
2025-09-25 18:14:27 +0530

    4 Triangle pattern

--- Patterns/Pyramid.java
+++ Patterns/Pyramid.java
@@ -1,11 +1,11 @@
 public class Pyramid {
     public static void main(String[] args) {
         int n =5;
-        for(int i =n;i>=1;i--){
-            for(int j =i;j>1;j--){
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=n-i;j++){
                 System.out.print(" ");
             }
-            for(int k=2*n-1;k>=2*i-1;k--){
+            for(int k=1;k<=2*i-1;k++){
                 System.out.print("*");
             }
             System.out.println();
---
+++ Patterns/Triangle4.java
@@ -0,0 +1,36 @@
+public class Triangle4 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i =1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i =1;i<=n;i++){
+             for(int j = n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k = n;k>=i;k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i =1;i<=n;i++){
+            for (int j =n-2;j>=i-1;j--){
+                System.out.print(" ");
+            }
+            for(int k = 1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
