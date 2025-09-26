0c9cc45f43e30bcb8c00682ba7ae39a7aad48d5f
avk0004 <vric.vignesh04@gmail.com>
2025-09-26 20:43:25 +0530

    circle area

---
+++ Self/Day 3/Circle.java
@@ -0,0 +1,8 @@
+import java.math.*;
+public class Circle{
+    public static void main(String[] args) {
+        float n = 25f;
+        double m = Math.PI*2*n;
+        System.out.printf("%12.6f", m);
+    }
+}
